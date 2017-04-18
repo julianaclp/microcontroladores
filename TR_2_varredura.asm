0c2e5c54af10aedee1a479e15aad8bf461c924cc
Juliana Pereira <Juliana Pereira>
2017-04-17 22:15:48 -0300

    Mudando o funcionamento do botão

--- TR_2_varredura.asm
+++ TR_2_varredura.asm
@@ -23,33 +23,39 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
-			mov.w	#WDTPW+WDTHOLD,&WDTCTL  ; Stop watchdog timer
-        	bic.w	#LOCKLPM5,PM5CTL0       ;Desbloqueia os Pinos I/O
-        	mov.b	#1, P1DIR
-        	mov.b	#0, P1OUT
-        	mov.b	#0x80, P9DIR
-        	mov.b	#0x00, P9OUT
-			bic.b	#BIT1, &P1DIR
-			bis.b	#BIT1, &P1REN
-			bis.b	#BIT1, &P1OUT
+            mov.w   #WDTPW+WDTHOLD,&WDTCTL  ; Stop watchdog timer
+            bic.w   #LOCKLPM5,PM5CTL0       ;Desbloqueia os Pinos I/O
+            mov.b   #1, P1DIR
+            mov.b   #0, P1OUT
+            mov.b   #0x80, P9DIR
+            mov.b   #0x00, P9OUT
+            bic.b   #BIT1, &P1DIR
+            bis.b   #BIT1, &P1REN
+            bis.b   #BIT1, &P1OUT
 
-loop 		bit.b	#BIT1, &P1IN			;lê estado do S1
-			jnz		off						;pula por padrão para o estado off
-			
-on 			bic.b	#1, P1OUT				;apaga o led P1.0				
-			mov.b	#0x20000, R5			;inicializa o registrador R5
-dec_on		dec		R5						;decrementa o R5 até 0
-			cmp		#0x00000, R5
-			jne		dec_on
-			xor.b	#0x80, P9OUT			
-			jmp 	loop
-off 		mov.b	#0x20000, R5
-			mov.b	#0x00, P9OUT
-dec_off		dec		R5
-			cmp 	#0x00000, R5
-			jne		dec_off
-			xor.b	#1, P1OUT
-			jmp 	loop
+loop        bit.b   #BIT1, &P1IN            ;lê estado do S1
+            jnz     off                     ;pula por padrão para o estado off
+
+on          bic.b   #1, P1OUT               ;apaga o led P1.0
+            mov.b   #0x20000, R5            ;inicializa o registrador R5
+dec_on      dec     R5                      ;decrementa o R5 até 0
+            cmp     #0x00000, R5
+            jne     dec_on
+            xor.b   #0x80, P9OUT
+            bit.b   #BIT1, &P1IN
+            jeq     off
+            jmp     on
+
+off         mov.b   #0x20000, R5
+            mov.b   #0x00, P9OUT
+dec_off     dec     R5
+            cmp     #0x00000, R5
+            jne     dec_off
+            xor.b   #1, P1OUT
+            jmp     loop
+            jmp     off
+
+
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
 ;-------------------------------------------------------------------------------
